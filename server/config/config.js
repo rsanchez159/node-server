481f8a217f428932283c2fe4568d1fb63e7c0a39
Ruben <smruben159@gmail.com>
2020-10-15 13:02:37 -0500

    Configurar correctamente conexion a mongo

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-if (process.env.NODE_ENV != 'dev')
+if (process.env.NODE_ENV = 'dev')
 {
     urlDB = 'mongodb://localhost:27017/AlbumPeru';
 }
